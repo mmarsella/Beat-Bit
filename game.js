91a498ec5a0c377ed2fb1cdf0b5f9524b6b20522
Mark Marsella <mmarsella114@g.rwu.edu>
2016-01-13 14:55:52 -0800

    children in groups of notes are recognized by overlap callbacks

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ function preload() {
     game.load.image('block', 'assets/Block-3.gif');
     game.load.image('platform', 'assets/platform.png');
     game.load.image('redNote', 'assets/redNote.png');
+    game.load.image('k_note', 'assets/k_note.png');
 
 }
 
@@ -21,6 +22,8 @@ var rectB;
 var rectC;
 var horiz;
 
+var start = 250;
+
 /* CONTROLS */
 var A,S,D,F,G,H,I,J,K,L;
 
@@ -29,10 +32,12 @@ var note;
 
 function create() {
 
-    notes = game.add.group();
+    game.autoCull = true;
 
+    console.log("GAME",game);
     A = game.input.keyboard.addKey(Phaser.Keyboard.A);
     L = game.input.keyboard.addKey(Phaser.Keyboard.L);
+    K = game.input.keyboard.addKey(Phaser.Keyboard.K);
 
     game.physics.setBoundsToWorld();
 
@@ -41,10 +46,6 @@ function create() {
     game.stage.backgroundColor = '#2d2d2d';
 
     // rectA = new Phaser.Rectangle(350, 50, 50, 200);
-
-
-
-
 
 
     /* Attempt to make a GROUP */ 
@@ -69,13 +70,36 @@ function create() {
     // console.log(rectB.x);
 
 
-    sprite1 = game.add.sprite(200, 5, 'redNote');
+
+
+    sprite1 = game.add.sprite(300, 5, 'redNote');
     sprite1.origHeight = sprite1.height;
     console.log("REDNOTE",sprite1);
 
     sprite2 = game.add.sprite(400, 50, 'redNote');
     sprite2.origHeight = sprite2.height;
 
+    //Add 5 k_notes, but 30 px apart on the y axis
+    notes = game.add.group();
+
+    var arr = [];
+
+    for(var i=0; i<4;i++){
+
+      note = notes.create(start+100*i, 5 - i*150, 'k_note')
+      note.origHeight = note.height;
+      // note = null;
+      // arr.push(note);
+
+    }
+
+    
+
+
+    game.add.tween(notes).to( { y: 1000 }, 5000, Phaser.Easing.Linear.None, true);
+    notes.enableBody = true;
+
+    console.log("NOTES!",notes);
 
 
 
@@ -85,21 +109,16 @@ function create() {
     sprite5 = game.add.sprite(200, 50, 'block');
 
     console.log("sprite1",sprite1);
-    // console.log("platform",platform);
+   
 
-    // console.log("rectA",rectA);
-
-    // notes.enableBody = true;
     // for(var i=0; i<3;i++){
     //   var note = notes.create(i*250,50,'redNote');
-    //   note.origHeight = note.body.height;
-    //   game.add.tween(note).to( { y: 600 }, 2000, Phaser.Easing.Linear.None, true);
     //   console.log("each NOTE:",note);
     // }
 
     // console.log("NOTES",notes);
 
-    game.physics.arcade.enable([sprite1,platform,sprite2,sprite4,sprite5,rectA,rectB], Phaser.Physics.ARCADE);
+    game.physics.arcade.enable([sprite1,platform,sprite2,sprite4,sprite5,rectA,rectB,notes], Phaser.Physics.ARCADE);
     
     // setSize --> (width,height, x, y)  top of canvas is y = 0, left x = 0
     rectA.body.setSize(30, 150, 200, 50); // set the size of the rectangle
@@ -147,16 +166,28 @@ function update() {
     // game.physics.arcade.overlap(notes, platform, overlapHandler, processHandler);
     // game.physics.arcade.overlap(rectA, platform, overlapHandler, processHandler2, this);
     
+    // console.log("CHILDREN",notes.children);
 
+    // for(var i=0; i<note.children.length; i++){
+    // }
 
-    game.physics.arcade.overlap(sprite1, platform, overlapHandler, processHandler, this);
-    game.physics.arcade.overlap(sprite2, platform, overlapHandler, processHandler, this);
+    //works without the 1st callback
+    game.physics.arcade.overlap(sprite1, platform, null, processHandler, this);
+    
+    // game.physics.arcade.overlap(notes, platform, null, processHandler_K2, this);
+    game.physics.arcade.overlap(notes.children, platform, null, processHandler_K, this);
+    // game.physics.arcade.overlap(notes.children[1], platform, null, processHandler, this);
+
+    game.physics.arcade.overlap(sprite2, platform, overlapHandler, processHandler2, this);
     // game.physics.arcade.overlap(sprite5, platform, overlapHandler, processHandler, this);
+    
 }
 
 
 
 function processHandler(obj1,obj2){
+
+  // console.log(obj1);
   // console.log("PROCESS HANDLER..");
   // if(obj1.y > obj2.y){
   //   // console.log("TRUE!");
@@ -175,11 +206,11 @@ function processHandler(obj1,obj2){
         console.log("DESTROYED!");
       }
     }else if(A.isDown){
-       obj1.destroy();
-       console.log("Right IS HIT!");
+       obj1.destroy(false,false);
+       console.log("Right IS HIT!",obj1);
       // game.stage.backgroundColor = '#006400';
-    }
     return false;
+    }
   }
 // }
 
@@ -213,11 +244,65 @@ function processHandler2(obj1,obj2){
   }
 }
 
+function processHandler_K(obj1,obj2){
+  
+  console.log("obj1_note",obj1);
+
+  if(obj1.y > obj2.y){
+    return true;
+  }else{
+
+    // BIG NOTE
+    if (K.isDown && obj1.origHeight >= 100)
+    {
+      obj1.height--;
+      console.log("LEFT IS HIT!");
+      // game.stage.backgroundColor = '#00FFFF';
+      if(obj1.height <= 10){
+        obj1.destroy();
+        console.log("DESTROYED!");
+      }
+    }else if(K.isDown){
+       obj1.destroy();
+        console.log("NOW NOTES", notes);
+
+    }
+    return false;
+  }
+}
+
+function processHandler_K2(obj1,obj2){
+  
+  console.log("obj1",obj1);
+  console.log("obj1 parent", obj1.parent)
+  debugger;
+  if(obj1.y > obj2.y){
+    return true;
+  }else{
+    debugger;
+    // BIG NOTE
+    if (K.isDown && obj1.origHeight >= 100)
+    {
+      obj1.height--;
+      console.log("LEFT IS HIT!");
+      // game.stage.backgroundColor = '#00FFFF';
+      if(obj1.height <= 10){
+        obj1.destroy();
+        console.log("DESTROYED!");
+      }
+    }else if(K.isDown){
+       obj1.destroy();
+
+        console.log("NOW NOTES", notes);
+
+    }
+    return false;
+  }
+}
+
 function overlapHandler (obj1, obj2) {
 
-    // game.stage.backgroundColor = '#992d2d';
     console.log("OVERLAP!");
-    // obj2.kill();
 }
 
 
@@ -240,9 +325,12 @@ function render() {
     // game.debug.geom(rectB, 'rgba(0,0,255,0.5)');
     // game.debug.geom(horiz, 'rgba(0,200,255,0.5)');
 
-    game.debug.body(sprite1);
+    if (note._bounds) {
+      game.debug.spriteInfo(note);
+    }
     game.debug.body(platform);
     game.debug.body(notes);
+    game.debug.text("Mark's game text", 280, 580);
 
 }
 
